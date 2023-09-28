2e6e889ad462109826123f27fb93bb10fbd05009
Hari <144226493+Hari1171@users.noreply.github.com>
2023-09-28 18:27:57 +0530

    Challenge-1(Search Product).py

--- Unit-3 Challenges/Challenge-1(Search Product).py
+++ Unit-3 Challenges/Challenge-1(Search Product).py
@@ -4,7 +4,7 @@ def linear_search_product(products, target_product):
         if product == target_product:
             indices.append(index)
     return indices
-product_list = ["mango", "cherry", "orange", "mango", "banana", "mango"]
-target_product = "mango"
+product_list = ["apple", "cherry", "orange", "apple", "banana", "apple"]
+target_product = "apple"
 result = linear_search_product(product_list, target_product)
 print(result)  
